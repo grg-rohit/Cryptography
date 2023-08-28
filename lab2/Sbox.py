253d83e907da27ab318871f0f4c9e73e01aba6a7
grg-rohit <81430349+grg-rohit@users.noreply.github.com>
2023-08-28 05:39:51 -0700

    Add files via upload
    lab2

---
+++ lab2/Sbox.py
@@ -0,0 +1,24 @@
+#Sbox 1 in DES
+def sbox1(input_bits):
+    sbox = (
+        # S-box 1
+        (14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7),
+        (0, 15, 7, 4, 14, 2, 13, 1, 10, 6, 12, 11, 9, 5, 3, 8),
+        (4, 1, 14, 8, 13, 6, 2, 11, 15, 12, 9, 7, 3, 10, 5, 0),
+        (15, 12, 8, 2, 4, 9, 1, 7, 5, 11, 3, 14, 10, 0, 6, 13)
+    )
+
+    row = int(input_bits[0] * 2 + input_bits[5])
+    col = int("".join(map(str, input_bits[1:5])), 2)
+
+    output_value = sbox[row][col]
+    output_bits = format(output_value, '04b')
+
+    return [int(bit) for bit in output_bits]
+
+# Example input: 6 bits
+input_bits = [1, 0, 0, 1, 1, 1]
+
+output_bits = sbox1(input_bits)
+print("Input bits:", input_bits)
+print("Output bits:", output_bits)
---
+++ lab2/expansionPbox.py
@@ -0,0 +1,29 @@
+#Expansion P-box
+import random as r
+
+expansion_Pbox = [32, 1, 2, 3, 4, 5, 4, 5,
+                  6, 7, 8, 9, 8, 9, 10, 11,
+                  12, 13, 12, 13, 14, 15, 16,
+                  17, 16, 17, 18, 19, 20, 21,
+                  20, 21, 22, 23, 24, 25, 24,
+                  25, 26, 27, 28, 29, 28, 29,
+                  30, 31, 32, 1]
+
+def setInputBlock():
+    input_block = ""
+    for _ in range(32):
+        input_block += str(r.randint(0,1))
+    return input_block
+
+def expandInput(inputText):
+    expandedText = ""
+    for index in expansion_Pbox:
+        expandedText += inputText[index-1]
+    return expandedText
+
+# inputText = setInputBlock()
+inputText = "10100110010001100110011001100110"
+expandedText = expandInput(inputText)
+
+print("Inital input: ", inputText)
+print("Output After Expansion: ", expandedText)
