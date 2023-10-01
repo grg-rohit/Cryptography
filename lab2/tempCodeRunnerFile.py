6ea3e6628e110e8106356de4619bd4512cfbdbaf
Rohit Gurung <grgrohit746@gmail.com>
2023-10-01 07:48:54 +0545

    lab2

--- lab2/tempCodeRunnerFile.py
+++ lab2/tempCodeRunnerFile.py
@@ -1,81 +1,62 @@
+def generate_round_keys(key):
+    # Permutation choice 1 (PC-1)
+    pc1 = [
+        57, 49, 41, 33, 25, 17, 9, 1,
+        58, 50, 42, 34, 26, 18, 10, 2,
+        59, 51, 43, 35, 27, 19, 11, 3,
+        60, 52, 44, 36, 63, 55, 47, 39,
+        31, 23, 15, 7, 62, 54, 46, 38,
+        30, 22, 14, 6, 61, 53, 45, 37,
+        29, 21, 13, 5, 28, 20, 12, 4
+    ]
 
-#Initial and Final Permutation Box demonstration
+    # Left and right halves of the key
+    left_key = key[:28]
+    right_key = key[28:]
 
-def permute(input_bits, permutation_box):
-    return ''.join(input_bits[i - 1] for i in permutation_box)
+    round_keys = []
 
-# Random 64-bit input plaintext (as binary string)
-input_bits = "1101010101101100110011110001111010100101001111100010110101110000"
+    for round_num in range(1, 17):
+        # Key schedule rotation amounts
+        rotations = 1 if round_num in {1, 2, 9, 16} else 2
 
-# Initial Permutation (IP) Box
-initial_permutation_box = (
-    58, 50, 42, 34, 26, 18, 10, 2,
-    60, 52, 44, 36, 28, 20, 12, 4,
-    62, 54, 46, 38, 30, 22, 14, 6,
-    64, 56, 48, 40, 32, 24, 16, 8,
-    57, 49, 41, 33, 25, 17, 9, 1,
-    59, 51, 43, 35, 27, 19, 11, 3,
-    61, 53, 45, 37, 29, 21, 13, 5,
-    63, 55, 47, 39, 31, 23, 15, 7
-)
+        # Perform key rotation
+        left_key = left_key[rotations:] + left_key[:rotations]
+        right_key = right_key[rotations:] + right_key[:rotations]
 
-# Final Permutation (FP) Box
-final_permutation_box = (
-    40, 8, 48, 16, 56, 24, 64, 32,
-    39, 7, 47, 15, 55, 23, 63, 31,
-    38, 6, 46, 14, 54, 22, 62, 30,
-    37, 5, 45, 13, 53, 21, 61, 29,
-    36, 4, 44, 12, 52, 20, 60, 28,
-    35, 3, 43, 11, 51, 19, 59, 27,
-    34, 2, 42, 10, 50, 18, 58, 26,
-    33, 1, 41, 9, 49, 17, 57, 25
-)
+        # Combine left and right halves
+        combined_key = left_key + right_key
 
-# Applying Initial Permutation
-permuted_bits = permute(input_bits, initial_permutation_box)
-print("Permuted Bits after Initial Permutation:", permuted_bits)
+        # Permutation choice 2 (PC-2) to generate subkey
+        pc2 = [
+            14, 17, 11, 24, 1, 5, 3, 28,
+            15, 6, 21, 10, 23, 19, 12, 4,
+            26, 8, 16, 7, 27, 20, 13, 2,
+            41, 52, 31, 37, 47, 55, 30, 40,
+            51, 45, 33, 48, 44, 49, 39, 56,
+            34, 53, 46, 42, 50, 36, 29, 32
+        ]
 
-# Applying Final Permutation
-encrypted_output = permute(permuted_bits, final_permutation_box)
-print("Encrypted Output after Final Permutation:", encrypted_output)
+        subkey = [combined_key[i - 1] for i in pc2]
+        round_keys.append(subkey)
 
-#Initial and Final Permutation Box demonstration
+    return round_keys
 
-def permute(input_bits, permutation_box):
-    return ''.join(input_bits[i - 1] for i in permutation_box)
+# Example 64-bit key
+key = [
+    1, 1, 0, 0, 1, 1, 0, 1,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    1, 1, 0, 0, 1, 1, 0, 0,
+    1, 1, 1, 1, 1, 1, 1, 1,
+    1, 0, 0, 0, 1, 1, 0, 1,
+    0, 1, 0, 1, 0, 1, 0, 1,
+    1, 1, 1, 1, 0, 0, 0, 0,
+    1, 0, 1, 0, 1, 0, 1, 0
+]
 
-# Random 64-bit input plaintext (as binary string)
-input_bits = "1101010101101100110011110001111010100101001111100010110101110000"
+# Generate round keys
+round_keys = generate_round_keys(key)
 
-# Initial Permutation (IP) Box
-initial_permutation_box = (
-    58, 50, 42, 34, 26, 18, 10, 2,
-    60, 52, 44, 36, 28, 20, 12, 4,
-    62, 54, 46, 38, 30, 22, 14, 6,
-    64, 56, 48, 40, 32, 24, 16, 8,
-    57, 49, 41, 33, 25, 17, 9, 1,
-    59, 51, 43, 35, 27, 19, 11, 3,
-    61, 53, 45, 37, 29, 21, 13, 5,
-    63, 55, 47, 39, 31, 23, 15, 7
-)
-
-# Final Permutation (FP) Box
-final_permutation_box = (
-    40, 8, 48, 16, 56, 24, 64, 32,
-    39, 7, 47, 15, 55, 23, 63, 31,
-    38, 6, 46, 14, 54, 22, 62, 30,
-    37, 5, 45, 13, 53, 21, 61, 29,
-    36, 4, 44, 12, 52, 20, 60, 28,
-    35, 3, 43, 11, 51, 19, 59, 27,
-    34, 2, 42, 10, 50, 18, 58, 26,
-    33, 1, 41, 9, 49, 17, 57, 25
-)
-
-# Applying Initial Permutation
-permuted_bits = permute(input_bits, initial_permutation_box)
-print("Permuted Bits after Initial Permutation:", permuted_bits)
-
-# Applying Final Permutation
-encrypted_output = permute(permuted_bits, final_permutation_box)
-print("Encrypted Output after Final Permutation:", encrypted_output)
-
+# Print the generated round keys
+for i, subkey in enumerate(round_keys):
+    print(f"Round {i+1} Subkey:", subkey)
